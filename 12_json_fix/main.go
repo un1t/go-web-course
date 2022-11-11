f4c26aca0adc16ca2066a7c13d5cccd37a797d79
Ilya Shalyapin <ishalyapin@gmail.com>
2022-11-11 23:13:11 +0500

    12_json_fix

---
+++ 12_json_fix/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type User struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+func main() {
+	http.HandleFunc("/user", UserHandler)
+	http.ListenAndServe(":3000", nil)
+}
+
+func WriteJson(w http.ResponseWriter, status int, v any) error {
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
+}
+
+func UserHandler(w http.ResponseWriter, r *http.Request) {
+	user1 := User{Name: "Andrey", Id: 555}
+	err := WriteJson(w, http.StatusOK, user1)
+
+	if err != nil {
+		WriteJson(w, http.StatusInternalServerError, map[string]any{
+			"ok":    false,
+			"error": err.Error(),
+		})
+		return
+	}
+}
