d40029859011fa73b09579a71c1ca2315c4407e5
Ilya Shalyapin <ishalyapin@gmail.com>
2022-11-11 23:13:57 +0500

    13 json request

--- 12_json_request/go.mod
+++
@@ -1,5 +0,0 @@
-module example
-
-go 1.19
-
-require github.com/gorilla/mux v1.8.0 // indirect
--- 13_json_request/main.go
+++ 13_json_request/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
 )
 
@@ -15,20 +16,21 @@ func main() {
 	http.ListenAndServe(":3000", nil)
 }
 
-func WriteJson(w http.ResponseWriter, status int, v any) error {
-	w.Header().Set("Content-Type", "application/json")
-	return json.NewEncoder(w).Encode(v)
-}
-
 func UserHandler(w http.ResponseWriter, r *http.Request) {
-	user1 := User{Name: "Andrey", Id: 555}
-	err := WriteJson(w, http.StatusOK, user1)
+	user1 := User{Name: "Ivan", Id: 555}
+	bytes, err := json.Marshal(user1)
+
+	err = errors.New("some error")
 
 	if err != nil {
-		WriteJson(w, http.StatusInternalServerError, map[string]any{
+		w.WriteHeader(http.StatusInternalServerError)
+		result := map[string]any{
 			"ok":    false,
 			"error": err.Error(),
-		})
+		}
+		json.NewEncoder(w).Encode(result)
 		return
 	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(bytes)
 }
