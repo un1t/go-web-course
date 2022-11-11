6d1b5393617cf7ebde26a0877dc44a7a1d439a37
Ilya Shalyapin <ishalyapin@gmail.com>
2022-11-11 23:44:45 +0500

    13 json request

--- 13_json_request/main.go
+++ 13_json_request/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"encoding/json"
-	"errors"
+	"fmt"
 	"net/http"
 )
 
@@ -16,21 +16,33 @@ func main() {
 	http.ListenAndServe(":3000", nil)
 }
 
+func WriteJson(w http.ResponseWriter, status int, v any) error {
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
+}
+
 func UserHandler(w http.ResponseWriter, r *http.Request) {
-	user1 := User{Name: "Ivan", Id: 555}
-	bytes, err := json.Marshal(user1)
-
-	err = errors.New("some error")
-
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		result := map[string]any{
+	if r.Method != http.MethodPost {
+		WriteJson(w, http.StatusMethodNotAllowed, map[string]any{
 			"ok":    false,
-			"error": err.Error(),
-		}
-		json.NewEncoder(w).Encode(result)
+			"error": "method not allowed",
+		})
 		return
 	}
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(bytes)
+
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		WriteJson(w, http.StatusInternalServerError, map[string]any{
+			"ok":    false,
+			"error": err.Error(),
+		})
+		return
+	}
+
+	fmt.Printf("user %v", user)
+
+	WriteJson(w, http.StatusOK, map[string]any{
+		"ok": true,
+	})
 }
