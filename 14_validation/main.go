baeea5e7258b2cd589087d937ab3aacf179b10bb
Ilya Shalyapin <ishalyapin@gmail.com>
2022-11-12 14:52:19 +0500

    14 validation

---
+++ 14_validation/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+	is "github.com/go-ozzo/ozzo-validation/v4/is"
+)
+
+type User struct {
+	Id    int    `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	Phone string `json:"phone"`
+}
+
+func (u User) Validate() error {
+	return validation.ValidateStruct(&u,
+		validation.Field(
+			&u.Name,
+			validation.Required.
+				Error("имя обязательно"),
+			validation.Length(1, 50).
+				Error("длинна имени должна быть от 1 до 50 символов"),
+		),
+		validation.Field(&u.Email, validation.Required, is.Email),
+		validation.Field(&u.Phone, is.E164, validation.Length(12, 12)),
+	)
+}
+
+func main() {
+	http.HandleFunc("/user", UserHandler)
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func WriteJson(w http.ResponseWriter, status int, v any) error {
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
+}
+
+func UserHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		WriteJson(w, http.StatusMethodNotAllowed, map[string]any{
+			"ok":    false,
+			"error": "method not allowed",
+		})
+		return
+	}
+
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		WriteJson(w, http.StatusInternalServerError, map[string]any{
+			"ok":    false,
+			"error": err.Error(),
+		})
+		return
+	}
+
+	err = user.Validate()
+	if err != nil {
+		WriteJson(w, http.StatusBadRequest, map[string]any{
+			"ok":    false,
+			"error": err.Error(),
+		})
+		return
+	}
+
+	fmt.Printf("user %v\n", user)
+
+	WriteJson(w, http.StatusOK, map[string]any{
+		"ok": true,
+	})
+}
